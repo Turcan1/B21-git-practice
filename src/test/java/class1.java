6beca34af05c57f66c18e6a097f37dbeebc7637f
Turcan1 <74077319+Turcan1@users.noreply.github.com>
2021-01-28 23:27:12 -0800

    update from intlj

--- src/test/java/class1.java
+++ src/test/java/class1.java
@@ -2,6 +2,10 @@ public class class1 {
     public static void main(String[] args) {
         System.out.println("etstts");
         System.out.println("test from github");
+        System.out.println("this");
+        System.out.println("is");
+        System.out.println("night");
+        System.out.println("test");
         
     }
 }
